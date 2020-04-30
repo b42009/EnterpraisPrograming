8dfd84503b3521ffc187d2ba7a34570c0f4a83d2
Miguel Cachia <Miguel.Cachia.b42009@mcast.edu.mt>
2020-04-30 13:56:35 +0200

    testingcommit

--- CACHIA_MIGUEL_EP/Controllers/HomeController.cs
+++ CACHIA_MIGUEL_EP/Controllers/HomeController.cs
@@ -7,7 +7,7 @@ using System.Web.Mvc;
 namespace CACHIA_MIGUEL_EP.Controllers
 {
     public class HomeController : Controller
-    {
+   
         public ActionResult Index()
         {
             return View();
